882429becde1930d8ae0c51e23901859c757c066
Tahoe Kim <70246100+tahoekim@users.noreply.github.com>
2021-12-06 02:07:31 -0600

    this is google sheet

--- pages/posts/inventory.js
+++ pages/posts/inventory.js
@@ -24,45 +24,12 @@ export default function inventory( { posts } ){
                     }}>
                     <center>
                     <p>
-                        <Link href="/posts/addinventory" passHref={true}><Button variant="outlined" startIcon={<Avatar src={'https://cdn1.iconfinder.com/data/icons/logistic-and-shipping-outline/32/logistic-product-parcel-increase-cargo-add-inventory-1024.png'} />}>
-                            Add Item to Inventory</Button></Link>
                     </p>
                     <main>
-                    <div className={utilStyles.container}>
-                    {posts.length == 0 ? (
-                        <p>No added posts</p>
-                    ) : (
-                            <InventoryList/>
-                    )}
-                    </div>
+                            <iframe width="500" height="1000" src="https://docs.google.com/spreadsheets/d/1bgGhoMhPIqlWcVbWjlqbCgSColwgzsRbDcm-PSXHkOk/edit?rm=minimal&widget=false#gid=0"></iframe>
                     </main>
                     </center> 
                 </section>
             </Layout>
         )
     }
-
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-    }
-
- const rows = [
-    createData('Size 12 hose clamps','Engine','Aircraft Spruce',3,'Radiator in/out'),
-    createData('Radiator','Engine','?',1,'Rear right side'),
-    createData('Oil tank','Engine','',1,'Center right behind firewall'),
-    createData('Gas tank','Engine','Manually machined',1,'Left side right behind firewall'),
-    createData('32 oz plastic water bottle', 'Engine', 'Nalgene', 1, "Behind radiator, right rear side"),
-    createData('Steel Pipe', 'Chassis', 'Aircraft Spruce', 5, 'See Notes'),
-    createData('Brake Tubing', 'Drive', 'NONE', 2, 'Brakes'),   
-    createData('Size 8 hose clamp', 'Other', 'Aircraft Spruce', 9, "Brakes and cooler"),
-    createData('Roll Bar Arms', 'Suspension', 'CNC Machined', 4, 'Either end of body'),
-  ];
-
-  export async function getServerSideProps(ctx) {
-
-    return {
-        props: {
-            posts: ['Message', 'M', 'T'],
-    },
-  }
-}
